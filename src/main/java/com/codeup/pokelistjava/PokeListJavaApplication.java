8c5e106893273a9271a839d3f494fc7746c2c6f7
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2022-02-14 12:53:19 -0600

    started new poke project. need to build db

---
+++ src/main/java/com/codeup/pokelistjava/PokeListJavaApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.pokelistjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeListJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeListJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/pokelistjava/PokeListJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.pokelistjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeListJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
